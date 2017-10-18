8b53fdc702be8dcbfdc24f2ac18d21736248dd75
Tomoki Kano <tomofuma@gmail.com>
2017-10-18 21:24:59 +0900

    add comments

--- tail.c
+++ tail.c
@@ -8,9 +8,10 @@
 
 #define BUFFER_SIZE 1028
 
+// 連結リストの要素
 struct node {
-	char line[BUFFER_SIZE];
-	struct node *next_p;
+    char line[BUFFER_SIZE]; // ファイルの一行を格納する
+    struct node *next_p; // 次のノードへのポインタ
 };
 
 int main(int argc, char *argv[]) {
@@ -18,16 +19,17 @@ int main(int argc, char *argv[]) {
     FILE* pFile = fopen(argv[1], "r");
     struct node *head, *tmp;
 
-	head = (struct node *) malloc( sizeof(struct node) );
-
+    // 長さ10の循環リストを定義
+    head = (struct node *) malloc( sizeof(struct node) );
     tmp = head;
-	for(int i = 10; i > 1; i--){
-		tmp->next_p = (struct node *) malloc(sizeof(struct node));
-		strcpy(tmp->line, "");
-		tmp = tmp->next_p;
-	}
-	tmp->next_p = head;
+    for(int i = 10; i > 1; i--){
+        tmp->next_p = (struct node *) malloc(sizeof(struct node));
+        strcpy(tmp->line, "");
+        tmp = tmp->next_p;
+    }
+    tmp->next_p = head;
 
+    // ファイルから一行ずつ読み，循環リストにいれる．
     tmp = head;
     char buffer[BUFFER_SIZE];
     while(fgets (buffer, BUFFER_SIZE, pFile) != NULL){
@@ -35,11 +37,12 @@ int main(int argc, char *argv[]) {
         tmp = tmp->next_p;
     }
 
-	head = tmp;
-	while(tmp->next_p != head){
-		printf("%s", (tmp->line));
-		tmp = tmp->next_p;
-	}
-	printf("%s", tmp->line);
-	return 0;
+    // 出力
+    head = tmp;
+    while(tmp->next_p != head){
+        printf("%s", (tmp->line));
+        tmp = tmp->next_p;
+    }
+    printf("%s", tmp->line);
+    return 0;
 }
