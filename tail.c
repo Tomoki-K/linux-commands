912b1b75367678e025ae96b544a43b129449860d
Tomoki Kano <tomofuma@gmail.com>
2017-10-24 10:06:57 +0900

    refactored

--- tail.c
+++ tail.c
@@ -1,34 +1,39 @@
 /*
-* Tomoki Kano
+* System Programming - mig-term report
+* Name: Tomoki Kano
 * ID: 1G151026
+*
+* GitHub: https://github.com/Tomoki-K/linux-commands
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
-#define BUFFER_SIZE 1024
+#define BUFF_SIZE 1024
+#define LN_COUNT 10
 
 // 連結リストの要素
 struct node {
-    char line[BUFFER_SIZE]; // ファイルの一行を格納する
+    char line[BUFF_SIZE]; // ファイルの一行を格納する
     struct node *next_p; // 次のノードへのポインタ
 };
 
 int main(int argc, char *argv[]) {
 
-    FILE* pFile = fopen(argv[1], "r");
+    // ファイルを開く
+    FILE *fp = fopen(argv[1], "r");
     int errnum = errno;
-    if (pFile == NULL) {
+    if (fp == NULL) {
         fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
         exit(1);
     }
-    struct node *head, *tmp;
 
     // 長さ10の循環リストを定義
+    struct node *head, *tmp;
     head = (struct node *) malloc( sizeof(struct node) );
     tmp = head;
-    for(int i = 10; i > 1; i--){
+    for(int i = LN_COUNT; i > 1; i--){
         tmp->next_p = (struct node *) malloc(sizeof(struct node));
         strcpy(tmp->line, "");
         tmp = tmp->next_p;
@@ -37,11 +42,12 @@ int main(int argc, char *argv[]) {
 
     // ファイルから一行ずつ読み，循環リストにいれる．
     tmp = head;
-    char buffer[BUFFER_SIZE];
-    while(fgets (buffer, BUFFER_SIZE, pFile) != NULL){
+    char buffer[BUFF_SIZE];
+    while(fgets (buffer, BUFF_SIZE, fp) != NULL){
         strcpy(tmp->line, buffer);
         tmp = tmp->next_p;
     }
+    fclose(fp);
 
     // 出力
     head = tmp;
